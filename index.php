5dbcabe6058edf07def5452c0cbda435b025b3fa
Colin But <colinbut@users.noreply.github.com>
2015-05-04 19:00:42 +0100

    index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Blog Engine v1.0</title>
+	</head>
+	<body>
+		<a href="addEntry.html">add blog entry</a>
+		<a href="#">view all</a>
+		<a href="#">search</a>
+	</body>
+</html>
