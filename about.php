5d406a96d35dc70c2dcb312bc1e7f19c63868841
Colin But <colinbut@users.noreply.github.com>
2015-05-17 20:20:02 +0100

    about page skeleton

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php require "dbAuth.php" ?>
+<!DOCTYPE html>
+<html>
+	<?php include "header.php" ?>
+	<body>
+		<?php include "navigation.php" ?>
+		<div id="content">
+			<div id="about_container">
+				<h1>About</h1>
+			</div>
+		</div>
+	</body>
+</html>
