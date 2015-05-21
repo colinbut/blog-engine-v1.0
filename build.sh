ba3826586e49a5fb03a683f67524051498cf5a30
Colin But <colinbut@users.noreply.github.com>
2015-05-21 22:32:18 +0100

    css for login page with image background

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd /Applications/XAMPP/htdocs/blog/;
+rm -rf *;
+cd /Applications/dev-sandbox/projects/personal-projects/blog-engine-v1.0/;
+cp -vR * /Applications/XAMPP/htdocs/blog/;
+cd /Applications/XAMPP/htdocs/blog/;
+chmod -R 777 *
+clear
--- login/login.php
+++ login/login.php
@@ -1,16 +1,26 @@
 <html>
     <head>
         <title>Blog Engine v1.0 - Login</title>
-        <link rel="stylesheet" type="text/css" href="css/login.css">
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
     </head>
     <body>
         <div id="form-container">
+            <p>Sign In</p>
             <form action="processLogin.php" method="POST" >
-                <label>username:</label>
-                <input type="text" name="USER"/>
-                <label>password:</label>
-                <input type="password" name="PW" />
-                <input type="submit" value="Login" />  
+                <table align="center">
+                    <tr>
+                        <td><label>Username:</label></td>
+                        <td><input type="text" name="USER"/></td>
+                    </tr>
+                    <tr>
+                        <td><label>Password:</label></td>
+                        <td><input type="password" name="PW" /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="Login" /></td>
+                    </tr>
+                </table>
             </form>
         </div>
     </body>
