18a97c106d0e6395e8959e5f229bedaab09c4ce8
Colin But <colinbut@users.noreply.github.com>
2015-07-03 21:56:28 +0100

    added simple parsley front end validation

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,14 @@
 <head>
-	<title>Blog Engine v1.0</title>
-	<!-- css -->
+	
+    <title>Blog Engine v1.0</title>
+	
+    <!-- css -->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/search.css">
     <link rel="stylesheet" type="text/css" href="css/addEntry.css">
     <link rel="stylesheet" type="text/css" href="css/about.css">
+    
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <Script src="js/parsley.min.js"></Script>
+    
 </head>
--- login/login.php
+++ login/login.php
@@ -7,15 +7,15 @@
         <h1 id="title">Blog Engine v1.0</h1>
         <div id="form-container">
             <p>Sign In</p>
-            <form action="processLogin.php" method="POST" >
+            <form action="processLogin.php" method="POST" data-parsley-validate>
                 <table align="center">
                     <tr>
                         <td><label>Username:</label></td>
-                        <td><input type="text" name="USER" placeholder="username"/></td>
+                        <td><input type="text" name="USER" placeholder="username" required/></td>
                     </tr>
                     <tr>
                         <td><label>Password:</label></td>
-                        <td><input type="password" name="PW" placeholder="password"/></td>
+                        <td><input type="password" name="PW" placeholder="password" required/></td>
                     </tr>
                     <tr>
                         <td></td>
